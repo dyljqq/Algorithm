67cacf886585930e6d4615ef4b70d755eee6d442
jiqinqiang <355086587@qq.com>
2022-12-18 18:17:41 +0800

    739. Daily Temperatures

--- 30-Day LeetCoding Challenge/739.swift
+++ 30-Day LeetCoding Challenge/739.swift
@@ -2,32 +2,29 @@
 //  739.swift
 //  Leetcode
 //
-//  Created by 季勤强 on 2021/11/13.
+//  Created by jiqinqiang on 2022/12/18.
 //
 
 import Foundation
 
-// 739. Daily Temperatures
-// https://leetcode.com/problems/daily-temperatures/
 class Leetcode739 {
+  
+  struct Temp {
+    let index: Int
+    let val: Int
+  }
+  
+  func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
+    var rs = Array(repeating: 0, count: temperatures.count)
+    var stack: [Temp] = []
     
-    // 使用递减栈
-    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
-        var rs: [Int] = Array(repeating: 0, count: temperatures.count)
-        var stack = [(Int, Int)]()
-        
-        for (index, item) in temperatures.enumerated() {
-            while let last = stack.last, last.1 < item {
-                rs[last.0] = index - last.0
-                stack.popLast()
-            }
-            stack.append((index, item))
-        }
-        return rs
+    for (index, temperature) in temperatures.enumerated() {
+      while let top = stack.last, top.val < temperature {
+        rs[top.index] = index - top.index
+        stack.popLast()
+      }
+      stack.append(Temp(index: index, val: temperature))
     }
-    
-    func test() {
-        print(dailyTemperatures([73,74,75,71,69,72,76,73]))
-    }
-    
+    return rs
+  }
 }
