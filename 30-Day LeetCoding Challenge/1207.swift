662c09e0913e8dc157a389255a61d11802b1a155
dyljqq <355086587@qq.com>
2024-01-17 09:54:51 +0800

    1207. Unique Number of Occurrences

--- 30-Day LeetCoding Challenge/1207.swift
+++ 30-Day LeetCoding Challenge/1207.swift
@@ -2,30 +2,18 @@
 //  1207.swift
 //  Leetcode
 //
-//  Created by jiqinqiang on 2022/11/30.
+//  Created by polaris dev on 2024/1/17.
 //
 
 import Foundation
 
-class Leetcode1207 {
-  
-  func uniqueOccurrences(_ arr: [Int]) -> Bool {
-    var hash: [Int: Int] = [:]
-    arr.forEach { hash[$0, default: 0] += 1 }
-    var set = Set<Int>()
-    for value in hash.values {
-      if set.contains(value) {
-        return false
-      }
-      set.insert(value)
+class Solution1207 {
+    func uniqueOccurrences(_ arr: [Int]) -> Bool {
+        var hash: [Int: Int] = [:]
+        for num in arr {
+            hash[num, default: 0] += 1
+        }
+        let values = hash.values
+        return values.count == Set(values).count
     }
-    return true
-  }
-  
-  func test() {
-    print(uniqueOccurrences([1,2,2,1,1,3]))
-    print(uniqueOccurrences([1,2]))
-    print(uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]))
-  }
-  
 }
