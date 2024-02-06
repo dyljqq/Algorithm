4b925844077f1c2572616c805543ff8f72c2980c
dyljqq <355086587@qq.com>
2024-02-06 10:13:20 +0800

    49. Group Anagrams

--- 30-Day LeetCoding Challenge/49.swift
+++ 30-Day LeetCoding Challenge/49.swift
@@ -2,53 +2,24 @@
 //  49.swift
 //  Leetcode
 //
-//  Created by jiqinqiang on 2022/10/28.
+//  Created by polaris dev on 2024/2/6.
 //
 
 import Foundation
 
-class Leetcode49 {
-  
-//  func groupAnagrams(_ strs: [String]) -> [[String]] {
-//    var hash: [String: [String]] = [:]
-//    strs.forEach { str in
-//      let sorted = String(str.sorted())
-//      if hash[sorted] == nil {
-//        hash[sorted] = [str]
-//      } else {
-//        hash[sorted]!.append(str)
-//      }
-//    }
-//    return hash.values.map { $0 }
-//  }
-  
-  func groupAnagrams(_ strs: [String]) -> [[String]] {
-    var dict: [String: [String]] = [:]
-    for str in strs {
-      var arr = Array(repeating: 0, count: 26)
-      for ch in str {
-        let diff = ch.asciiValue! - Character("a").asciiValue!
-        arr[Int(diff)] += 1
-      }
-      
-      var key = ""
-      for i in 0..<26 {
-        guard arr[i] > 0 else { continue }
-        let ch = Character(UnicodeScalar(Character("a").asciiValue! + UInt8(i)))
-        key += "\(ch)\(arr[i])"
-      }
-      
-      if dict[key] == nil {
-        dict[key] = [str]
-      } else {
-        dict[key]!.append(str)
-      }
+class Solution49 {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var hash: [String: [String]] = [:]
+        strs.forEach { str in
+            let key = String(str.sorted())
+            hash[key, default: []].append(str)
+        }
+        return hash.values.map { $0 }
     }
-    return dict.values.map { $0 }
-  }
-  
-  func test() {
-    print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
-  }
-  
+    
+    func test() {
+        print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+        print(groupAnagrams([]))
+        print(groupAnagrams(["a"]))
+    }
 }
--- README.md
+++ README.md
@@ -260,6 +260,7 @@
 * [1043. Partition Array for Maximum Sum](https://leetcode.com/problems/partition-array-for-maximum-sum/)
 * [76. Minimum Window Substring](https://leetcode.com/problems/minimum-window-substring/)
 * [387. First Unique Character in a String](https://leetcode.com/problems/first-unique-character-in-a-string/)
+* [49. Group Anagrams](https://leetcode.com/problems/group-anagrams/)
 
 ### 使用swift解题
 
